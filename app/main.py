f915aaf8ed7cd563d05c362641468995c056c735
roseab <roseab.developer@gmail.com>
2025-11-26 09:51:31 -0300

    agrego descripcion al swagger

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from app.routes import items_routes, convert_routes
 
-app = FastAPI()
+app = FastAPI(title="Minecraft Trade API",
+    description="""
+API para convertir ítems, minerales, alimentos y materiales de Minecraft  
+en base al valor de 1 diamante.
+
+Permite:
+- Consultar valores relativos
+- Convertir ítems entre sí
+- Manejar estaciones de trabajo, alimentos y materiales
+""",
+    version="1.0.0",
+    contact={
+        "name": "Ricardo",
+        "email": "roseab.developer@gmail.com",
+    })
 
 # Registrar rutas
 app.include_router(items_routes.router)
--- app/routes/convert_routes.py
+++ app/routes/convert_routes.py
@@ -4,7 +4,7 @@ from app.services.convert_service import convert_items
 
 router = APIRouter()
 
-@router.post("/convert")
+@router.post("/convert", tags=["Conversion"], summary="Convierte un item a otro", description="Convierte un item fuente a un item destino basado en el valor relativo a 1 diamante.")
 def convert(data: ConvertRequest):
     result = convert_items(data.source, data.target, data.amount)
     if result is None:
