3d26c1305a774a2428dd6f6fe293dea550fb7c3a
roseab <roseab.developer@gmail.com>
2025-11-25 23:25:36 -0300

    corrijo pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,9 @@ module.exports = {
       name: "fastapi-backend-minecraft",
       script: "./venv/bin/uvicorn",
       args: "app.main:app --host 0.0.0.0 --port 8005",
-      watch: false,
+      interpreter: "none",   // <- ESTO ES CLAVE
       autorestart: true,
+      watch: false,
       max_memory_restart: "500M",
       env: {
         ENV: "production"
